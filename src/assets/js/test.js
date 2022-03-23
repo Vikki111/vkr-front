ea98d32980fa3a15c13a3c79d0efcb9cc206fded
Vikki111 <osnova_forall@mail.ru>
2022-03-23 10:01:42 +0400

    fixed creating graph for new students

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -145,14 +145,18 @@ $(document).ready(function() {
     var studentId = window.location.href.split('graph-editor/')[1];
     ajax("http://localhost:8080/students/graph/"+studentId)
     .then(function(result) {
-      const obj = JSON.parse(result);
-      recover(obj);
+      if (result == "") {
+          mainFunc()
+      } else {
+          const obj = JSON.parse(result);
+          recover(obj);
+      }
     })
     .catch(function() {
     });
 
 //data3 = {"nodes":[{"size":50,"label":"1","x":54.51593031012319,"y":-203.4484449595114,"dX":0,"dY":0,"id":1,"read_cam0:size":20,"read_cam0:x":0,"read_cam0:y":0,"renderer1:x":445.5,"renderer1:y":479.5,"renderer1:size":20}],"edges":[]};
-
+function mainFunc() {
     graph = new sigma({
         renderer: {
             container: document.getElementById('graph-container'),
@@ -177,6 +181,251 @@ $(document).ready(function() {
         }
     });
 
+    //евент нажатия правой кнопкой по ребру
+            graph.bind('rightClickEdge', function(e) {
+                console.log(e.data.edge.id);
+                if (e.data.edge.isSelected) {
+                    e.data.edge.color = '#ccc';
+                    e.data.edge.isSelected = false;
+                    if (greenEdgeIds[0] == e.data.edge.id) {
+                        greenEdgeIds[0] = null;
+                        $('#inputEdge').val('');
+                    }
+                } else {
+                    e.data.edge.color = "#0A0"; //зеленый
+                    e.data.edge.isSelected = true;
+                    if (greenEdgeIds[0] == null) {
+                        greenEdgeIds[0] = e.data.edge.id;
+                    } else {
+                        graph.graph.edges().forEach(function(edge) {
+                            if (edge.id != e.data.edge.id) {
+                                edge.color = '#ccc';
+                                edge.isSelected = false;
+                            }
+                        });
+                        greenEdgeIds[0] = e.data.edge.id;
+                    }
+                    $('#inputEdge').val(e.data.edge.label);
+                }
+                graph.refresh();
+            });
+
+            //евент нажатия правой кнопкой по вершине
+            graph.bind('rightClickNode', function(e) {
+                console.log(e.data.node.id);
+                if (e.data.node.isSelected) { //если вершина была выделена, то убираем выделение
+                    e.data.node.color = "#000";
+                    e.data.node.isSelected = false;
+                    if (redNodeIds[0] == e.data.node.id) {
+                        redNodeIds[0] = null;
+                    }
+                } else { //если была не выделена - выделяем
+                    e.data.node.color = "#f00"; //красный
+                    e.data.node.isSelected = true;
+                    if (redNodeIds[0] == null) {
+                        redNodeIds[0] = e.data.node.id;
+                    } else {
+                        if(existsEdge(redNodeIds[0] + "-" + e.data.node.id) == false) { //если не было такого ребра, то добавляем
+                            graph.graph.addEdge({ //добавление связей
+                                id: redNodeIds[0] + "-" + e.data.node.id,
+                                source: redNodeIds[0],
+                                size: 12,
+                                target: e.data.node.id,
+                                color: '#ccc',
+                                hover_color: '#000',
+                                type: "arrow"
+                            });
+                        }
+                        if(existsEdge(e.data.node.id + "-" + redNodeIds[0])) { //если было обратное ребро, то удаляем его
+                            graph.graph.dropEdge(e.data.node.id + "-" + redNodeIds[0]);
+                        }
+                        graph.graph.nodes().forEach(function(node) {
+                            if (node.id == e.data.node.id || node.id == redNodeIds[0]) { //убираем выделение вершины
+                                node.color = '#000';
+                                node.isSelected = false;
+                            }
+                        });
+                        redNodeIds[0] = null;
+                    }
+                }
+                graph.refresh();
+            });
+
+            //евент нажатия кнопкой по одной и той же вершине вершине
+            graph.bind('clickNode', function(e) {
+                console.log(e.data.node.id);
+                if (e.data.node.isSelected) { //если вершина была выделена, то добавляем ребро, само на себя
+                    e.data.node.color = "#000";
+                    e.data.node.isSelected = false;
+                    if (redNodeIds[0] == e.data.node.id) {
+                       if(existsEdge(redNodeIds[0] + "-" + e.data.node.id) == false) { //если не было такого ребра, то добавляем
+                           graph.graph.addEdge({ //добавление связей
+                               id: redNodeIds[0] + "-" + e.data.node.id,
+                               source: redNodeIds[0],
+                               size: 12,
+                               target: e.data.node.id,
+                               color: '#ccc',
+                               hover_color: '#000',
+                               type: 'curve'
+                           });
+                         }
+                         redNodeIds[0] = null;
+                    }
+                }
+                graph.refresh();
+            });
+
+                //евент именения названия ребра
+                $('#changeEdgeNameButton').click(function() {
+                    console.log($('#inputEdge').val());
+                    if(greenEdgeIds[0] != null) {
+                        graph.graph.edges().forEach(function(edge) {
+                            if (edge.id == greenEdgeIds[0]) {
+                                edge.label = $('#inputEdge').val();
+                            }
+                        });
+                    }
+                    graph.refresh();
+
+            //        edgeLabels[selectedEdgeId] = $('#inputEdge').val();
+            //        console.log(Object.keys(edgeLabels));
+            //        console.log(Object.values(edgeLabels));
+                });
+
+
+            //берем тэг конструктора графов
+            var dom = document.querySelector('#graph-container canvas:last-child');
+
+            //убираем контекстное меню по нажатю на ПКМ
+            dom.addEventListener('contextmenu', event => event.preventDefault());
+
+            //добавление новой вершины
+            dom.addEventListener('dblclick', function(e) {
+
+                var x,
+                    y,
+                    p,
+                    id,
+                    neighbors;
+
+                x = sigma.utils.getX(e) - dom.offsetWidth / 2;
+                y = sigma.utils.getY(e) - dom.offsetHeight / 2;
+
+                p = graph.camera.cameraPosition(x, y);
+                x = p.x;
+                y = p.y;
+                var test = ++nId;
+
+                graph.graph.addNode({
+                    id: test,
+                    size: 50,
+                    label: test + "",
+                    x: x + Math.random() / 10,
+                    y: y + Math.random() / 10,
+                    dX: 0,
+                    dY: 0,
+                });
+                graph.refresh();
+            }, false);
+
+            //удаление вершин
+            $('html').keyup(function(e) {
+                if (e.keyCode == 46) { //если нажата клавиша delete
+                    if (redNodeIds[0] != null) {
+                        graph.graph.dropNode(redNodeIds[0]);
+                        redNodeIds[0] = null;
+                    }
+                    if (greenEdgeIds[0] != null) {
+                        graph.graph.dropEdge(greenEdgeIds[0]);
+                        greenEdgeIds[0] = null;
+                    }
+                    graph.refresh();
+                }
+            });
+
+            //распечатка джейсона графа
+                $('#save').click(function() {
+            //    console.log(JSON.stringify({nodes: graph.graph.nodes(), edges: graph.graph.edges()}));
+                    savejson();
+                });
+
+                //POST запрос на бэк, чтобы сохранить json с графом
+                async function savejson() {
+                var studentId = window.location.href.split('graph-editor/')[1];
+                    let response = await fetch('http://localhost:8080/savejson', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8',
+                            'student-id':studentId
+                        },
+                        body: JSON.stringify({
+                            nodes: graph.graph.nodes(),
+                            edges: graph.graph.edges()
+                        })
+                    });
+
+                    let result = await response.json();
+                    console.log(result);
+                }
+
+
+        var dragListener = sigma.plugins.dragNodes(graph, graph.renderers[0]);
+
+        //  graph = new sigma('container');
+        processData();
+
+        //TODO непонятно
+        //постоянное чтение из тэга input и выполнение processData
+        let timeout;
+        $('#input').on('input', function() {
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                processData();
+            }, 500);
+        });
+
+        //TODO бесполезен сейчас
+        //евент двойного клика по ребру
+        graph.bind('doubleClickEdge', function(e) {
+            console.log(e.data.edge.id);
+            selectedEdgeId = e.data.edge.id;
+        });
+
+
+
+        //функция проверки наличия вершины по id
+        function existsNode(id) {
+            graph.graph.nodes().forEach(function(node) {
+                if (node.id == id) {
+                    return true;
+                }
+            });
+            return false;
+        }
+
+        //функция проверки наличия ребра по id
+        function existsEdge(id) {
+            var flag = false;
+            graph.graph.edges().forEach(function(edge) {
+                if (edge.id == id) {
+                    flag = true;
+                }
+            });
+            return flag;
+        }
+
+        //функция нахождения противоположного ребра
+        function existsInverseEdge(id) {
+                var flag = false;
+                var arrayOfStrings = id.split("-");
+                graph.graph.edges().forEach(function(edge) {
+                    if (edge.id == arrayOfStrings[0]) {
+                        flag = true;
+                    }
+                });
+                return flag;
+            }
+}
 function recover(data) {
         graph = null;
         graph = new sigma({
